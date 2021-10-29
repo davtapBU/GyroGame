adc008ba17b6be8ce863aae047f37c191577adc5
David Tapia <flawie@davids-mbp.home>
2021-10-29 19:50:16 -0400

    Made it so that there is haptic feedback when the player collides with an object

--- GyroGame/GyroGame/GameScene.swift
+++ GyroGame/GyroGame/GameScene.swift
@@ -13,7 +13,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var sprite = SKSpriteNode()
     var motionManager = CMMotionManager()
     var destX:CGFloat  = 0.0
-    var destY:CGFloat  = 0.0
     
     //Layers
     var foregroundNode : SKNode!
@@ -21,18 +20,64 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //Scale factor
     var scaleFactor: CGFloat!
     
-    //Player
+    //Player/Objects
     var player: SKNode!
+    var pallet: SKNode!
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = .white
+        
+        //Add gravity
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        physicsWorld.contactDelegate = self
+        
+        scaleFactor = self.size.width / 320.0
+        
+        //Foreground
+        foregroundNode = SKNode()
+        addChild(foregroundNode)
+        
+        //Add the player
+        player = createPlayer()
+        foregroundNode.addChild(player)
+        
+        //Add the pallet
+        pallet = createPallet()
+        foregroundNode.addChild(pallet)
+
+        if motionManager.isAccelerometerAvailable {
+            // 2
+            motionManager.accelerometerUpdateInterval = 0.01
+            motionManager.startAccelerometerUpdates(to: .main) {
+                (data, error) in
+                guard let data = data, error == nil else {
+                    return
+                }
+
+                // 3
+                let currentX = self.sprite.position.x
+                self.destX = currentX + CGFloat(data.acceleration.x * 500)
+            }
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        //Cause haptic feedback upon impact of 2 objects
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
+        generator.impactOccurred()
+    }
     
     func createPlayer() -> SKNode {
         let playerNode = SKNode()
-        playerNode.position = CGPoint(x: self.size.width / 2, y: self.size.height)
+        playerNode.position = CGPoint(x: self.size.width / 2, y: self.size.height/2)
         
         let sprite = SKSpriteNode(imageNamed: "bulogo")
         sprite.setScale(0.15)
         playerNode.addChild(sprite)
         
         playerNode.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        playerNode.physicsBody?.usesPreciseCollisionDetection = true
+        playerNode.physicsBody?.contactTestBitMask = 1
         playerNode.physicsBody?.isDynamic = true
         playerNode.physicsBody?.allowsRotation = true
         playerNode.physicsBody?.restitution = 1.0
@@ -47,11 +92,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let palletNode = SKNode()
         palletNode.position = CGPoint(x: self.size.width / 2, y: 80.0)
         
-        let sprite = SKSpriteNode(imageNamed: "pallet")
-        sprite.setScale(0.20)
+        let sprite = SKSpriteNode(imageNamed: "Platform")
+        sprite.setScale(2.00)
         palletNode.addChild(sprite)
         
-        palletNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 15.0, height: 8.0))
+        palletNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: sprite.size.width, height: sprite.size.height))
         palletNode.physicsBody?.isDynamic = false
         palletNode.physicsBody?.allowsRotation = false
         palletNode.physicsBody?.restitution = 1.0
@@ -59,51 +104,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         palletNode.physicsBody?.angularDamping = 0.0
         palletNode.physicsBody?.linearDamping = 0.0
         
+        palletNode.physicsBody?.contactTestBitMask = 1
+        
         return palletNode
     }
     
-    override func didMove(to view: SKView) {
-        backgroundColor = .white
-        
-        //Add gravity
-        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
-        
-        scaleFactor = self.size.width / 320.0
-        
-        //Foreground
-        foregroundNode = SKNode()
-        addChild(foregroundNode)
-        
-        //Add the player
-        player = createPlayer()
-        foregroundNode.addChild(player)
-        
-        //Add the pallet
-        foregroundNode.addChild(createPallet())
-
-        if motionManager.isAccelerometerAvailable {
-            // 2
-            motionManager.accelerometerUpdateInterval = 0.01
-            motionManager.startAccelerometerUpdates(to: .main) {
-                (data, error) in
-                guard let data = data, error == nil else {
-                    return
-                }
-
-                // 3
-                let currentX = self.sprite.position.x
-                let currentY = self.sprite.position.y
-                self.destX = currentX + CGFloat(data.acceleration.x * 500)
-                self.destY = currentY + CGFloat(data.acceleration.y * 500)
-            }
-        }
-    }
-    
     override func update(_ currentTime: TimeInterval) {
+        //Move the player and pallet in the direction of the gyroscope
         let actionX = SKAction.moveTo(x: destX, duration: 1)
-        let actionY = SKAction.moveTo(y: destY, duration: 1)
-       // sprite.run(actionX)
-            //sprite.run(actionY)
+        player.run(actionX)
+        pallet.run(actionX)
     }
     
 }
